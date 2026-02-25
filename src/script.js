a6cd876eae6bdcb09160e037a98995f291c1793e
Ingrid Michelle <mnegreiro14@gmail.com>
2026-02-24 22:13:10 -0300

    Aula 1 - Variáveis em JavaScript

---
+++ src/script.js
@@ -0,0 +1,16 @@
+// Exemplos de variavel let
+
+console.log("Testando JavaScript");
+
+let nome= 'Ingrid';
+
+let nome2= 'Michelle';
+
+console.log(nome)
+console.log(nome2)
+
+
+// Exemplos de variável const
+const times = ['Corinthians','São Paulo', 'Santos'];
+
+console.log(times);
