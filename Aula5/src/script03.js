766411cf8726507b1d0185a430654d3f8d92b964
Ingrid Michelle <mnegreiro14@gmail.com>
2026-03-02 22:13:22 -0300

    Aula 5 - Laço de Repetição

---
+++ Aula5/src/script03.js
@@ -0,0 +1,23 @@
+// Introdução Laços de Repetição
+
+
+// Exemplo de For Loop
+// for (let valor = 0; valor < 10; valor++){
+    // console.log(valor);
+// }
+
+// Exemplo de While Loop
+// let acumuladora = 0;
+
+// while(acumuladora < 10){
+    // console.log("o valor da acumuladora agora é de: " + acumuladora);
+    // acumuladora  = acumuladora + 1;
+// }
+
+// Exemplo de DO While Loop
+let acumuladora2 = 0;
+
+do{
+     console.log("o valor da acumuladora agora é de: " + acumuladora2);
+     acumuladora2 = acumuladora2 + 1;
+} while (acumuladora2 <= 10);
