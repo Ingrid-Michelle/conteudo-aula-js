4e5484d524763b7b809f0f377b666c73ec2fe708
Ingrid Michelle <mnegreiro14@gmail.com>
2026-03-23 13:43:06 -0300

    Aula 15 - Estudando Manipulações de Elementos no HTML

---
+++ Aula15/src/script03.js
@@ -0,0 +1,7 @@
+// Utilizando o innerHTML
+document.querySelector(".mudandoElemento").innerHTML = "Ingrid Michelle";
+
+// Alterando atributos
+const elemento = document.querySelector(".foto");
+
+elemento.src = "Frame1.png";
