53a7ba47a0ec4e63e6f918867e4c819f75201e9a
Ingrid Michelle <mnegreiro14@gmail.com>
2026-03-23 19:26:34 -0300

    Aula 18 - Estudando evento de carregamento

--- Aula18/src/script.js
+++ Aula18/src/script.js
@@ -1,43 +1,9 @@
-// Exemplo de animação com JavaScript
-function animacao() {
-  const elementoAnimado = document.querySelector("#animacao");
-  
-  let posicaoX = 0;
-  let posicaoY = 0;
-  let direcao = "right";
+// Utilizando o evento de carregamento load
+const img = document.querySelector(".minha-imagem");
 
-  const limite = 150;
 
-  setInterval(frame, 5);
 
-  function frame() {
-    if (direcao === "right") {
-      posicaoX = posicaoX + 1;
-      if (posicaoX >= limite) {
-        direcao = "down";
-      }
-    } else if (direcao === "down") {
-      posicaoY = posicaoY + 1;
-      if (posicaoY >= limite) {
-        direcao = "left";
-      }
-    } else if (direcao === "left") {
-      posicaoX = posicaoX - 1;
-      if (posicaoX <= 0) {
-        direcao = "up";
-      }
-    } else if (direcao === "up") {
-      posicaoY = posicaoY - 1;
-      if (posicaoY <= 0) {
-        direcao = "right";
-      }
-    }
-
-    elementoAnimado.style.left = posicaoX + "px";
-    elementoAnimado.style.top = posicaoY + "px";
-
-  }
-
-}
-
-animacao();
+img.addEventListener("load", function() {
+  const elemento = document.querySelector(".info-carregamento");
+  elemento.innerHTML = "A imagem foi carregada."
+})
